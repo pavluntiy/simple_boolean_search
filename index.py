e517cb56bef4c15d16c1886e5f024895ffa73e80
pavel <itasarom@gmail.com>
2016-04-06 15:39:46 +0300

    Started refactoring: wrapped indexer into an object

--- index.py
+++ index.py
@@ -10,121 +10,105 @@ from varbyteencoder import VarByteEncoder
 
 
 
-def read_docs(pathlist):
-    for path in pathlist:
-        for doc in DocumentStreamReader(path):
-            yield (doc.url, doc.text)
+class Indexer:
 
-word_re = re.compile(r'\w+', flags = re.UNICODE)
-def get_word_list(text):
-    # words = Counter()
-    words = re.findall(word_re, text)
-    words = map(lambda w: w.lower(), words)
-    return Counter(words)
+    def read_docs(self, pathlist):
+        for path in pathlist:
+            for doc in DocumentStreamReader(path):
+                yield (doc.url, doc.text)
 
-
-def get_words(docs):
-    urls = {}
-    words_in_docs = []
-    total_words = Counter()
-    for url, text in docs:     
-        urls[len(urls)] = url
-        cur_words = get_word_list(text)
-        words_in_docs.append(cur_words.keys()) 
-        total_words.update(cur_words)
-
-    return urls, words_in_docs, total_words
-
-def get_pairs(words_in_docs, total_words):
-    word_to_idx = {}
-    idx_to_word = {}
-
-    for idx, word in enumerate(total_words.keys()):
-        idx_to_word[idx] = word
-        word_to_idx[word] = idx
-
-    pairs = []
-    for doc_id, words in enumerate(words_in_docs):
-        for word in words:
-            pairs.append((word_to_idx[word], doc_id))
-
-    return word_to_idx, idx_to_word, pairs
+    word_re = re.compile(r'\w+', flags = re.UNICODE)
+    def get_word_list(self, text):
+        words = re.findall(self.word_re, text)
+        words = map(lambda w: w.lower(), words)
+        return Counter(words)
 
 
-def build_index(pairs, word_to_idx):
-    index_file = open("./data/index.txt", "w")
-    pairs = sorted(pairs)
+    def get_words(self, docs):
+        urls = {}
+        words_in_docs = []
+        self.total_words = Counter()
+        for url, text in docs:     
+            urls[len(urls)] = url
+            cur_words = self.get_word_list(text)
+            words_in_docs.append(cur_words.keys()) 
+            self.total_words.update(cur_words)
 
-    # prev = None
-    word_index = defaultdict(lambda: [])
-    for pair in pairs:
-        word = pair[0]
-        doc_id = pair[1]
-        # if word != prev and prev is not None:
-            # index_file.write(str(index.iteritems()))
-            # word_index = defaultdict(lambda: [])
-        word_index[word].append(doc_id)
-        # prev = word
+        self.idx_to_urls = urls
+        self.words_in_docs = words_in_docs 
 
-    # index_file.write(str(index.iteritems()))
-    new_word_ids = {}
+    def get_pairs(self):
+        self.word_to_idx = {}
+        self.idx_to_word = {}
 
-    _lens = {}
-    varbyte = VarByteEncoder()
-    for word, doc_ids in word_index.iteritems():
-        # index_file.write("{0} ".format(word))
+        for idx, word in enumerate(self.total_words.keys()):
+            self.idx_to_word[idx] = word
+            self.word_to_idx[word] = idx
 
-        new_word_ids[word] = index_file.tell()
-        index_file.write(varbyte.encode(sorted(doc_ids)))
-        _lens[word] = index_file.tell() - new_word_ids[word]
-        # print new_word_ids[word]
-        # for doc_id in sorted(doc_ids):
-        #     index_file.write("{0} ".format(doc_id))
-        # index_file.write("\n")
-    index_file.close()
+        pairs = []
+        for doc_id, words in enumerate(self.words_in_docs):
+            for word in words:
+                pairs.append((self.word_to_idx[word], doc_id))
 
-    # print lens
-    lens = {}
-    for word in word_to_idx:
-        word_id = word_to_idx[word]
-        new_word_id = new_word_ids[word_id]
-        lens[new_word_id] = _lens[word_id]
-        word_to_idx[word] = new_word_id
+        return pairs
+
+
+    def build_index(self, pairs):
+        index_file = open("./data/index.txt", "w")
+        pairs = sorted(pairs)
+
+        word_index = defaultdict(lambda: [])
+        for pair in pairs:
+            word = pair[0]
+            doc_id = pair[1]
+            word_index[word].append(doc_id)
+
+        new_word_ids = {}
+
+        _lens = {}
+        encoder = VarByteEncoder()
+        for word, doc_ids in word_index.iteritems():
+            new_word_ids[word] = index_file.tell()
+            index_file.write(encoder.encode(sorted(doc_ids)))
+            _lens[word] = index_file.tell() - new_word_ids[word]
+
+        index_file.close()
+
+        lens = {}
+        for word in self.word_to_idx:
+            word_id = self.word_to_idx[word]
+            new_word_id = new_word_ids[word_id]
+            lens[new_word_id] = _lens[word_id]
+            self.word_to_idx[word] = new_word_id
 
 
 
-    dict_file = open("./data/dict.txt", "w")
-    for word, idx in word_to_idx.iteritems():
-            # print lens[word_to_idx[word]]
-            dict_file.write(u"{0} {1} {2}\n".format(word, idx, lens[word_to_idx[word]]).encode("utf-8"))
-    dict_file.close()
+        dict_file = open("./data/dict.txt", "w")
+        for word, idx in self.word_to_idx.iteritems():
+                dict_file.write(u"{0} {1} {2}\n".format(word, idx, lens[self.word_to_idx[word]]).encode("utf-8"))
+        dict_file.close()
 
-    # print max(word_to_idx.values())
-
-    return new_word_ids, word_index
+        return new_word_ids, word_index
 
 
 
-def build_dict(docs):
-    idx_to_urls, words_in_docs, total_words = get_words(docs)
-    word_to_idx, idx_to_word, pairs = get_pairs(words_in_docs, total_words)
-    new_word_ids, word_index = build_index(pairs, word_to_idx)
+    def build_dict(self, docs):
+        self.get_words(docs)
+        pairs = self.get_pairs()
+        self.build_index(pairs)
 
 
-
-
-
-
-    urls_file = open("./data/urls.txt", "w")
-    for idx, url in idx_to_urls.iteritems():
-            urls_file.write(u"{0} {1}\n".format(idx, url).encode("utf-8"))
-    urls_file.close()
+        urls_file = open("./data/urls.txt", "w")
+        for idx, url in self.idx_to_urls.iteritems():
+                urls_file.write(u"{0} {1}\n".format(idx, url).encode("utf-8"))
+        urls_file.close()
 
     
 def main():
     os.system("mkdir tmp")
     os.system("mkdir data")
-    build_dict(read_docs(sys.argv[2:]))
+    indexer = Indexer()
+    indexer.build_dict(indexer.read_docs(sys.argv[2:]))
     os.system("rm -rf tmp")
 
 
