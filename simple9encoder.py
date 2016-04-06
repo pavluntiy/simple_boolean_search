e79682e86718e0d17d3315f798f4755c4d229d70
pavel <itasarom@gmail.com>
2016-04-06 17:51:56 +0300

    Encodes, I believe

--- simple9encoder.py
+++ simple9encoder.py
@@ -1,51 +1,129 @@
 import struct
+
+import math
+
+class BitstreamWriter:
+    def __init__(self):
+        self.nbits  = 0
+        self.curbyte = 0
+        self.vbytes = []
+
+    def add(self, x):
+        self.curbyte |= x << (8-1 - (self.nbits % 8))
+        self.nbits += 1
+
+        if self.nbits % 8 == 0:
+            self.vbytes.append(chr(self.curbyte))
+            self.curbyte = 0
+
+    def getbytes(self):
+        if self.nbits & 7 == 0:
+            return "".join(self.vbytes)
+
+        return "".join(self.vbytes) + chr(self.curbyte)
+
+
+class BitstreamReader:
+    def __init__(self, blob):
+        self.blob = blob
+        self.pos  = 0
+
+    def get(self):
+        ibyte = self.pos / 8
+        ibit  = self.pos & 7
+
+        self.pos += 1
+        return (ord(self.blob[ibyte]) & (1 << (7 - ibit))) >> (7 - ibit)
+
+    def finished(self):
+        return self.pos >= len(self.blob) * 8
+
+
+
 class Simple9Encoder:
     name = "simple9"
-        
-    def encode(self, xs):
-        res = ""
+
+    values = [28, 14, 9, 7, 5, 4, 3, 2, 1]
+
+    def can_pack(self, xs, n):
+        if len(xs) != n:
+            return False
+
+        max_log = 0
         for x in xs:
-            res += self.encode_one(x)
-        return res
+            max_log = max(max_log, math.ceil(math.log(x, 2)))
 
-    def encode_one(self, x):
-        rem = 0
-        res = ""
+        # print max_log
+        if n * max_log <= 28:
+            return True
 
-        i = 0
-        while x >= 0:
-            rem = x % 128
-            if i == 0:
-                rem += 128
-            res = struct.pack('B', rem) + res
-            x /= 128
-            i += 1
-            if x == 0:
-                break
-        return res
+        return False
+
+    def get_signature(self, n):
+        return bin(self.values.index(n))[2:].zfill(4)
+
+    def get_n(self, signature):
+        return self.values[int(signature, 2)]
+
+    def get_fill_len(self, n):
+        return 28/n
+
+    def encode_batch(self, xs):
+        # print len(xs)
+        resstr = self.get_signature(len(xs))
+        fill_len = self.get_fill_len(len(xs))
+
+        for x in xs:
+            resstr += bin(x)[2:].zfill(fill_len)
+            # print resstr
+
+        while len(resstr) < 32:
+            resstr += '0'
+
+        # print resstr
+
+        return resstr
+
+    def write_str(self, s): 
+        for c in s:
+            if c == '1':
+                self.writer.add(1)
+            else:
+                self.writer.add(0)
+                  
+
+    def encode(self, xs):
+
+        self.writer = BitstreamWriter()
+    
+        while len(xs) > 0:
+            for n in self.values:
+                if self.can_pack(xs[:n], n):
+                    break
+
+            resstr = self.encode_batch(xs[:n])
+            print resstr
+            self.write_str(resstr)
+
+            xs = xs[n:]
+
+
+        return self.writer.getbytes()
+
+
             
     def decode(self, blob):
+        raise ValueError()
 
-        res = []
-        i = len(blob) - 1
-        while i >= 0:
-            x = 0
-            cur = struct.unpack('B', blob[i])[0]
-            # print bin(ord(blob[i]))
-            x += cur - 128
-            i -= 1
-            cur = struct.unpack('B', blob[i])[0]
-            if cur < 128:
-                d = 128
-                # print x
-                while cur < 128:
-                    x += cur * d
-                    i -= 1
-                    cur = struct.unpack('B', blob[i])[0]
-                    
-                    d *= 128
-                    # print x
+def main():
+    encoder = Simple9Encoder()
+    # for it in encoder.values:
+    #     print encoder.get_signature(it)
+    res =  encoder.encode([1, 2, 3, 16, 19, 11])
+    print map(lambda x: bin(ord(x))[2:].zfill(8), res)
 
-            res.append(x)
+    # print encoder.can_pack([1, 1, 1, 1, 1], 5)
 
-        return list(reversed(res))
+
+if __name__ == "__main__":
+    main()
